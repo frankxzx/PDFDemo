3c0e8a4df4a0d5b24dfaf70c72cb414fd5d4b043
frank.xu <frank.xu@touch-spring.com>
2018-11-12 07:51:25 +0800

    init

---
+++ PDFDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDFDemo
+//
+//  Created by Xuzixiang on 2018/11/9.
+//  Copyright © 2018 frankxzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PDFDemo/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  PDFDemo
+//
+//  Created by Xuzixiang on 2018/11/9.
+//  Copyright © 2018 frankxzx. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BNHtmlPdfKit.h"
+
+@interface ViewController () <BNHtmlPdfKitDelegate>
+
+@property(nonatomic, strong) BNHtmlPdfKit *pdf;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Override point for customization after application launch.
+    NSString *path = [[NSBundle mainBundle]pathForResource:@"Header" ofType:@"html"];
+    NSString *path2 = [[NSBundle mainBundle]pathForResource:@"Content" ofType:@"html"];
+    NSString *headerHtmlString = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    NSString *contentHtmlString = [NSString stringWithContentsOfFile:path2 encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *folderPath = [documentPath stringByAppendingPathComponent:@"pdf"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if(![fileManager fileExistsAtPath:folderPath]) {
+        [fileManager createDirectoryAtPath:folderPath withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    NSString *localSavedPath = [folderPath stringByAppendingPathComponent:@"render.pdf"];
+    
+//    BNHtmlPdfKit *pdf = [[BNHtmlPdfKit alloc]init];
+//    pdf.delegate = self;
+//    [pdf saveHtmlAsPdf:headerHtmlString toFile:localSavedPath];
+    
+    BNHtmlPdfKit *pdf = [BNHtmlPdfKit saveHtmlAsPdf:contentHtmlString toFile:localSavedPath pageHeaderHtml:headerHtmlString pageFooterHtml:nil success:^(NSString *filename) {
+        
+    } failure:^(NSError *error) {
+        
+    }];
+    
+    self.pdf = pdf;
+    
+
+}
+
+
+@end
